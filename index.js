a02ee485149a0fe1ab7c57bf9cce3e8c0e803c8a
Muhammad Nazmul Alam <nazmul_2020s@yahoo.com>
2020-07-29 15:25:57 +0200

    edited js project

---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+       const a = 10,
+        b = 3,
+        c= 15;
+     const d= '5';
+        e = 'my name',
+        f = 'is simon';
+        console.log(a+b);
+        console.log(a+c);
+        console.log(e+ ' ' +f);
+        console.log(a+d);
+
+</script>
